fc14d7bda1c17f6588b79995d1beb6e84f3a4d01
brandonthiessen <brandonthiessen2005@gmail.com>
2026-07-02 21:44:32 -0400

    feat: create basic logging macros

---
+++ include/log.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <format>
+
+enum class LogLevel {
+    OFF = 0,
+    ERROR = 1,
+    INFO = 2,
+    DEBUG = 3
+};
+
+class Logger {
+public:
+    inline static LogLevel level = LogLevel::INFO;
+};
+
+#define LOG_ERROR(fmt, ...) \
+    do { if (Logger::level >= LogLevel::ERROR) \
+        std::cout << std::format("[ERROR] " fmt "\n", __VA_ARGS__); \
+    } while(0)
+
+#define LOG_INFO(fmt, ...) \
+    do { if (Logger::level >= LogLevel::INFO) \
+        std::cout << std::format("[INFO] " fmt "\n", __VA_ARGS__); \
+    } while(0)
+
+#define LOG_DEBUG(fmt, ...) \
+    do { if (Logger::level >= LogLevel::DEBUG) \
+        std::cout << std::format("[DEBUG] " fmt "\n", __VA_ARGS__); \
+    } while(0)
