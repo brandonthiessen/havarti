61647e2f8f1006d5af478e3e033b72589589c139
brandonthiessen <brandonthiessen2005@gmail.com>
2026-07-02 21:49:15 -0400

    feat: create synthetic order generator

---
+++ include/order_generator.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "order.h"
+
+static constexpr int64_t MID_PRICE = 1000; // 10.00
+
+#include <random>
+
+class OrderGenerator {
+    public:
+        OrderGenerator(int seed);
+        OrderGenerator(int seed, int64_t price_low, int64_t price_high, int qty_low, int qty_high);
+        Order next_order();
+        std::vector<Order> next_orders(int n);
+    private:
+        std::mt19937 rng;
+        std::uniform_int_distribution<int> side_dist;
+        std::uniform_int_distribution<int64_t> price_dist;
+        std::uniform_int_distribution<int> qty_dist;
+
+        int next_id = 1;
+};
---
+++ src/order_generator.cpp
@@ -0,0 +1,28 @@
+#include "order_generator.h"
+
+OrderGenerator::OrderGenerator(int seed)
+    : rng(seed), side_dist(0, 1), price_dist(MID_PRICE - 50, MID_PRICE + 50), qty_dist(1, 100), next_id(1)
+{}
+
+OrderGenerator::OrderGenerator(int seed, int64_t price_low, int64_t price_high, int qty_low, int qty_high)
+    : rng(seed), side_dist(0, 1), price_dist(price_low, price_high), qty_dist(qty_low, qty_high), next_id(1)
+{}
+
+Order
+OrderGenerator::next_order()
+{
+    return Order{next_id++, side_dist(rng) == 0 ? Side::BUY : Side::SELL, price_dist(rng), qty_dist(rng)};
+}
+
+std::vector<Order>
+OrderGenerator::next_orders(int n)
+{
+    std::vector<Order> orders;
+    orders.reserve(n);
+
+    for (int i = 0; i < n; i++) {
+        orders.push_back(next_order());
+    }
+
+    return orders;
+}
